b7e723df9483aa0065f35a4e29af20e0fb97e658
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-07-12 21:50:56 +0800

    announcement feature

--- resources/views/announcement/announcement.blade.php
+++ resources/views/announcement/announcement.blade.php
@@ -1,4 +1,9 @@
 @include('partials.__header')
 @include('components.nav')
 <h1>ANNOUNCEMENTS</h1>
+@foreach ($announcements as $announcement)
+<p>{{$announcement -> announcement_title}}</p>
+@endforeach
+
+<!-- KATE CHECK SA ANNOUNCEMENT CONTROLLER UNSAY MGA DATA NGA GIPANG PASA DIRI, SAMPLE NANA ANG TITLE -->
 @include('partials.__footer')
