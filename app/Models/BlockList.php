3e468ae584c5e5a008e60905178ff1c79a14907f
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-07-28 21:32:57 +0800

    added model for blocklists

---
+++ app/Models/BlockList.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class BlockList extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'homeowner_id',
+        'admin_id',
+        'user_name',
+        'first_name',
+        'last_name',
+        'contact_number',
+        'blocked_date',
+        'block_reason',
+        'visit_status',
+    ];
+    
+    public function homeowner()
+    {
+        return $this->belongsTo(User::class, 'homeowner_id');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+}
