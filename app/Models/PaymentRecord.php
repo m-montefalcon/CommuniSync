ce986667cb120b75060a22da0dc7440a96320a01
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-08-28 20:55:08 +0800

    created DB for PaymentRecords

---
+++ app/Models/PaymentRecord.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentRecord extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'admin_id',
+        'homeowner_id',
+        'payment_date',
+        'payment_amount',
+        'transaction_number',
+        'notes'
+    ];
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+
+}
