2fb2644762fe517c0ef2577d4a323f79a66616e6
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-11-23 12:38:49 +0800

    picture asset users, pagination css

--- resources/views/credentialsForm/editHomeownerForm.blade.php
+++ resources/views/credentialsForm/editHomeownerForm.blade.php
@@ -20,7 +20,7 @@
           <div class="user-profile">
             <div class="card-profile">
               @if ($homeowner->photo)
-                <img src="http://127.0.0.1:8000/storage/{{ Auth::user()->photo }}" alt="User Photo">
+              <img src="{{ asset('storage/' . $homeowner->photo) }}" alt="User Photo">
               @else
                 <img src="{{ asset('Assets/default-user-profile.jpg') }}" alt="Default Photo">
               @endif
