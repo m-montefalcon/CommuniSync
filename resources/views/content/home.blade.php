7d020e3e3a70946134b0560728125f90e48fcd16
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-11-26 18:32:33 +0800

    added backend for dashboard web

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -2,6 +2,16 @@
 @include('components.nav')
 <h1>Home</h1>
 
+<div>
+    <p>Number of Visits this month: {{ $visitCount }}</p>
+    <p>Number of Complaints this month: {{ $complaintsCount }}</p>
+    <p>Number of Payments this month: {{ $paymentCount }}</p>
+    <p>Number of Homeowners overall: {{ $homeownerCount }}</p>
+    <p>Number of Announcements this month: {{ $announcementCount }}</p>
+    <p>Total amount of monthly due this month: ₱{{ $totalAmountPayment }}</p>
+    <p>Number of users: {{ $totalUsersCount }}</p>
+
+
+</div>
 
 @include('partials.__footer')
-
