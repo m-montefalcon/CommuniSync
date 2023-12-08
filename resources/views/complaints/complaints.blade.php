7bfd92ac59d634974fdb016c52849e1aa072c7db
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-12-09 02:35:46 +0800

    complaint photo update

--- resources/views/complaints/complaints.blade.php
+++ resources/views/complaints/complaints.blade.php
@@ -188,7 +188,7 @@
             $('#complaintStatus').val(currentStatus);
             $('#complaintUpdates').html(updatesHtml);
             $('#complaintSendFrom').val(sendFrom);
-            $('#complaintPhoto').attr('src', '{{ asset("storage/" . ($complaint->complaint_photo ?? "default.jpg")) }}');
+            $('#complaintPhoto').attr('src', '{{ asset("storage/") }}' + '/' + photo);
             var formAction = '/api/admin/complaint/update/' + id;
             $('#complaintForm').attr('action', formAction);
 
